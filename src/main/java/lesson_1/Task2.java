52e006eed817348dfbc7b2fcd9b0bbd2cd13fe8b
Shamil Aliev <inhuh005@gmail.com>
2017-08-22 13:13:30 +0300

    Решение второй задачи 2

--- src/main/java/lesson_1/Task2.java
+++ src/main/java/lesson_1/Task2.java
@@ -1,4 +1,24 @@
 package lesson_1;
 
+import java.util.Scanner;
+
 public class Task2 {
+
+    public static void main(String[] args) {
+        System.out.println("Исходный текст:");
+        String str = "Я хороший разработчик автотестов";
+        System.out.println(str+"\n");
+
+
+        //замена слова "хороший"
+        System.out.println("Текст после замены слова:");
+        str =  str.replace("хороший","гениальный");
+        System.out.println(str+"\n");
+
+        // получение новый строки
+        System.out.println("новая строка:");
+        String subStr = str.substring(13,24);
+        System.out.println(subStr);
+    }
+
 }
