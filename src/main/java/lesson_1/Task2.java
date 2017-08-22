c61e9c56e483db3acbec6294456237209cc60e33
Shamil Aliev <inhuh005@gmail.com>
2017-08-22 14:13:40 +0300

    Решение второй задачи: ввод слова c клавиатуры

--- src/main/java/lesson_1/Task2.java
+++ src/main/java/lesson_1/Task2.java
@@ -1,23 +1,43 @@
 package lesson_1;
 
 
+import java.util.Scanner;
+
 public class Task2 {
 
+    private static int wordLength;
+
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         System.out.println("Исходный текст:");
         String str = "Я хороший разработчик автотестов";
-        System.out.println(str+"\n");
+        System.out.println(str + "\n");
 
 
-        //замена слова "хороший"
+        //замена слова
+        System.out.println("Введите новое слово:");
+        String newWord = scanner.nextLine();
+        wordLength = newWord.length();
+        String newStr = replaceWord(str, newWord);
         System.out.println("Текст после замены слова:");
-        str =  str.replace("хороший","гениальный");
-        System.out.println(str+"\n");
+        System.out.println(newStr + "\n");
+
+        scanner.close();
 
         // получение новый строки
-        System.out.println("новая строка:");
-        String subStr = str.substring(13,24);
-        System.out.println(subStr);
+        System.out.println("Новая строка:");
+        printSubStr(newStr);
+    }
+
+    private static String replaceWord(String str,String newWord) {
+        return str.replace("хороший", newWord);
+    }
+
+    private static void printSubStr(String str) {
+        int beginIndex = wordLength+3;
+        int endIndex = beginIndex + 11;
+        System.out.println(str.substring(beginIndex, endIndex));
     }
 
 }
